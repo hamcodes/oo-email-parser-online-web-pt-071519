67828a1e0dd0c49e664376e0364eae284bf03a9f
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-12 13:45:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,10 +3,10 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :email_addresses
-  email = []
+  attr_accessor :email
+  
   def initialize(email)   
-    email_addresses = ''
+    
   end
 
   def parse
